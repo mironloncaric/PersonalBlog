463b092e0746338b3829295342f3985ed7744d38
Miron Lončarić <mironloncaric@debian-BULLSEYE-live-builder-AMD64>
2021-11-24 15:37:13 +0100

    Added a welcome screen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
 
 const router = require("./router/router.js");
 
@@ -8,9 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.send("<h3>Welcome</h3>");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
